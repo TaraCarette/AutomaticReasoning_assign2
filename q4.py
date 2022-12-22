16fa20c925821690fe3023ae16f07c50c1376ecb
TaraCarette <terra.the.great@gmail.com>
2022-12-22 14:36:37 -0500

    fix crossing number convention

--- q4.py
+++ q4.py
@@ -8,7 +8,7 @@ maxPerBoat = 2
 minPerBoat = 1
 
 
-maxBoatCrossings = 10
+maxBoatCrossings = 2
 
 # define types based on: 
 # how many there are
@@ -27,8 +27,8 @@ beingsTotal = sum([x["num"] for x in beingsToCross])
 
 
 # setting up booleans for each being on each side for each timestep
-leftSide = [[] for x in range(maxBoatCrossings)]
-rightSide = [[] for x in range(maxBoatCrossings)]
+leftSide = [[] for x in range(maxBoatCrossings + 1)]
+rightSide = [[] for x in range(maxBoatCrossings + 1)]
 
 # begin with all on the left, none on the right
 for b in beingsToCross:
@@ -36,7 +36,7 @@ for b in beingsToCross:
 	rightSide[0] = rightSide[0] + [BoolVal(False) for x in range(b["num"])]
 
 # rest will be assigned
-for t in range(1, maxBoatCrossings):
+for t in range(1, maxBoatCrossings + 1):
 	for b in beingsToCross:
 		leftSide[t] = leftSide[t] + [Bool(f'{b["name"]}{x}_step{t}_left') for x in range(b["num"])]
 		rightSide[t] = rightSide[t] + [Bool(f'{b["name"]}{x}_step{t}_right') for x in range(b["num"])]
@@ -44,7 +44,7 @@ for t in range(1, maxBoatCrossings):
 
 # over all possible crossings, says if boat on left
 # so false means boat is on the right
-boatOnLeft = [BoolVal(x % 2 == 0) for x in range(maxBoatCrossings)]
+boatOnLeft = [BoolVal(x % 2 == 0) for x in range(maxBoatCrossings + 1)]
 
 # for now, no rules about who can be on the boat together, since for now always with man, switch later
 
@@ -65,13 +65,13 @@ def getBeingIndexes(name, beingsToCross):
 
 # a being can only be one one side of the river at once
 # also must be on at least 1 side
-solver.add(And([Sum(leftSide[t][b], rightSide[t][b]) == 1 for b in range(len(leftSide[t])) for t in range(len(leftSide))]))
+solver.add(And([Sum(leftSide[t][b], rightSide[t][b]) == 1 for b in range(len(leftSide[t])) for t in range(maxBoatCrossings + 1)]))
 
 
 # there must be a point where everyone is on the right side
 # (as problem is symmetric, will not result in a failure if allowed to keep moving, so can ignore if takes
 # less turns than given to achieve this)
-solver.add(Or([And(rightSide[t]) for t in range(len(rightSide))]))
+# solver.add(Or([And(rightSide[t]) for t in range(len(rightSide))]))
 
 # if a being is required on the boat, it must switch sides every turn
 # get the index of every required being
@@ -82,34 +82,36 @@ for b in beingsToCross:
 		neededIndex = neededIndex + getBeingIndexes(b["name"], beingsToCross)
 
 # make sure those beings switch sides
-solver.add([(And(Implies(leftSide[t - 1][b], rightSide[t][b]), Implies(rightSide[t - 1][b], leftSide[t][b]))) for b in neededIndex for t in range(1, len(rightSide))])
+solver.add([(And(Implies(leftSide[t - 1][b], rightSide[t][b]), Implies(rightSide[t - 1][b], leftSide[t][b]))) for b in neededIndex for t in range(1, maxBoatCrossings + 1)])
 
 
 # the side that has the boat for this timestep will have
 # at least the minimum change to false, and no more than the maximum become false
-solver.add([Implies(boatOnLeft[t - 1], Sum([leftSide[t - 1][b] != leftSide[t][b] for b in range(beingsTotal)]) <= maxPerBoat) for t in range(1, maxBoatCrossings)])
-solver.add([Implies(boatOnLeft[t - 1], Sum([leftSide[t - 1][b] != leftSide[t][b] for b in range(beingsTotal)]) >= minPerBoat) for t in range(1, maxBoatCrossings)])
+solver.add([Implies(boatOnLeft[t - 1], Sum([leftSide[t - 1][b] != leftSide[t][b] for b in range(beingsTotal)]) <= maxPerBoat) for t in range(1, maxBoatCrossings + 1)])
+solver.add([Implies(boatOnLeft[t - 1], Sum([leftSide[t - 1][b] != leftSide[t][b] for b in range(beingsTotal)]) >= minPerBoat) for t in range(1, maxBoatCrossings + 1)])
 
-solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) <= maxPerBoat) for t in range(1, maxBoatCrossings)])
-solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) >= minPerBoat) for t in range(1, maxBoatCrossings)])
+solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) <= maxPerBoat) for t in range(1, maxBoatCrossings + 1)])
+solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) >= minPerBoat) for t in range(1, maxBoatCrossings + 1)])
 
 # if boat did not move to this side at this timestep, nothing can switch to true
 # (basically, only boat movement can allows being movement)
-solver.add([Implies(Not(boatOnLeft[t]), Not(Or([And(Not(leftSide[t - 1][b]), leftSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings)])
-solver.add([Implies(boatOnLeft[t], Not(Or([And(Not(rightSide[t - 1][b]), rightSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings)])
+solver.add([Implies(Not(boatOnLeft[t]), Not(Or([And(Not(leftSide[t - 1][b]), leftSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings + 1)])
+solver.add([Implies(boatOnLeft[t], Not(Or([And(Not(rightSide[t - 1][b]), rightSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings + 1)])
 
 
 # some beings cannot be left alone with others safely
 counter = 0
 for b1 in beingsToCross:
-	print(b1["name"])
 	if b1["cannotOutnumber"] != []:
 		b1Index = getBeingIndexes(b1["name"], beingsToCross)
 
 		for b2 in b1["cannotOutnumber"]:
 			b2Index = getBeingIndexes(b2, beingsToCross)
 
-			solver.add([Sum([leftSide[t][b] for b in b1Index]) <= Sum([leftSide[t][b] for b in b2Index]) for t in range(maxBoatCrossings)])
+			# Sum([leftSide[t][b] for b in b1Index]) <= Sum([leftSide[t][b] for b in b2Index])
+			# if boat not there to supervise, then make sure no bad pairings
+			# solver.add([Implies(Not(boatOnLeft[t]), Sum([leftSide[t][b] for b in b1Index]) < Sum([leftSide[t][b] for b in b2Index])) for t in range(maxBoatCrossings + 1)])
+			# print([Implies(Not(boatOnLeft[t]), Sum([leftSide[t][b] for b in b1Index]) < Sum([leftSide[t][b] for b in b2Index])) for t in range(maxBoatCrossings + 1)])
 
 print(solver.check())
 
@@ -124,7 +126,7 @@ for being in beingsToCross:
 		print(being["name"] + str(i) + " LEFT")
 
 
-for t in range(1, maxBoatCrossings):
+for t in range(1, maxBoatCrossings + 1):
 	print("--------------------")
 	print("Turn " + str(t))
 
