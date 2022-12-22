05f25fd48ea999a30055a5b65c2f11dd7b133991
TaraCarette <terra.the.great@gmail.com>
2022-12-22 14:16:09 -0500

    basic no outnumbering rules

--- q4.py
+++ q4.py
@@ -16,10 +16,10 @@ maxBoatCrossings = 10
 # which group they cannot outnumber
 # required to be on boat (like the man guiding it)
 
-man = {"name": "man", "num": 1, "weight": 1, "cannotOutnumberOrEqual": [], "required": True}
-wolf = {"name": "wolf", "num": 1, "weight": 1, "cannotOutnumberOrEqual": ["goat"], "required": False}
-goat = {"name": "goat", "num": 1, "weight": 1, "cannotOutnumberOrEqual": ["cabbage"], "required": False}
-cabbage = {"name": "cabbage", "num": 1, "weight": 1, "cannotOutnumberOrEqual": [], "required": False}
+man = {"name": "man", "num": 1, "weight": 1, "cannotOutnumber": [], "required": True}
+wolf = {"name": "wolf", "num": 1, "weight": 1, "cannotOutnumber": ["goat"], "required": False}
+goat = {"name": "goat", "num": 1, "weight": 1, "cannotOutnumber": ["cabbage"], "required": False}
+cabbage = {"name": "cabbage", "num": 1, "weight": 1, "cannotOutnumber": [], "required": False}
 beingsToCross = [man, wolf, goat, cabbage]
 
 # get the total number of beings to cross
@@ -51,6 +51,18 @@ boatOnLeft = [BoolVal(x % 2 == 0) for x in range(maxBoatCrossings)]
 solver = Solver()
 
 
+def getBeingIndexes(name, beingsToCross):
+	requiredIndex = []
+	counter = 0
+	for b in beingsToCross:
+		if b["name"] == name:
+			for i in range(b["num"]):
+				requiredIndex.append(counter + i)
+
+		counter += b["num"]
+
+	return requiredIndex
+
 # a being can only be one one side of the river at once
 # also must be on at least 1 side
 solver.add(And([Sum(leftSide[t][b], rightSide[t][b]) == 1 for b in range(len(leftSide[t])) for t in range(len(leftSide))]))
@@ -63,53 +75,15 @@ solver.add(Or([And(rightSide[t]) for t in range(len(rightSide))]))
 
 # if a being is required on the boat, it must switch sides every turn
 # get the index of every required being
-requiredIndex = []
+neededIndex = []
 counter = 0
 for b in beingsToCross:
 	if b["required"]:
-		for i in range(b["num"]):
-			requiredIndex.append(counter + i)
-
-	counter += b["num"]
+		neededIndex = neededIndex + getBeingIndexes(b["name"], beingsToCross)
 
 # make sure those beings switch sides
-solver.add([(And(Implies(leftSide[t - 1][b], rightSide[t][b]), Implies(rightSide[t - 1][b], leftSide[t][b]))) for b in requiredIndex for t in range(1, len(rightSide))])
+solver.add([(And(Implies(leftSide[t - 1][b], rightSide[t][b]), Implies(rightSide[t - 1][b], leftSide[t][b]))) for b in neededIndex for t in range(1, len(rightSide))])
 
-
-
-# only the max weight of beings switch sides (as boat can only hold that much)
-# print(Sum([leftSide[t - 1] != rightSide[t]]))
-# every submix possible of creatures
-# only those whose combo weight is acceptable
-
-# possibleBoatCombos = itertools.combinations(rightSide[1], maxPerBoat)
-# print(possibleBoatCombos)
-# quit()
-possibleBoatCombos = []
-for t in range(1, len(rightSide)):
-	for subset in itertools.combinations(leftSide[t], maxPerBoat):
-		ind1 = leftSide[t].index(subset[0])
-		ind2 = leftSide[t].index(subset[1])
-		possibleBoatCombos.append(And(subset[0] == Not(leftSide[t - 1][ind1]), subset[1] == Not(leftSide[t - 1][ind2])))
-
-
-# print(possibleBoatCombos)
-
-# boatCombos = []
-# for boatMember in range(maxPerBoat):
-# 	boatCombos.append([boatMember])
-# 	for i in range(beingsTotal):
-# 		boatCombos[boatMember].append(i)
-
-# print(boatCombos)
-
-# 	for j in range(beingsTotal):
-# 		print(i, j)
-
-
-# last time step not being exactly equal to itself implies that
-# one of the following 2 flipped
-# solver.add([Implies(Or([leftSide[t][b] != leftSide[t - 1][b] for b in range(beingsTotal)]), Sum(possibleBoatCombos) == 1) for t in range(1, len(leftSide))])
 
 # the side that has the boat for this timestep will have
 # at least the minimum change to false, and no more than the maximum become false
@@ -119,14 +93,23 @@ solver.add([Implies(boatOnLeft[t - 1], Sum([leftSide[t - 1][b] != leftSide[t][b]
 solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) <= maxPerBoat) for t in range(1, maxBoatCrossings)])
 solver.add([Implies(Not(boatOnLeft[t - 1]), Sum([rightSide[t - 1][b] != rightSide[t][b] for b in range(beingsTotal)]) >= minPerBoat) for t in range(1, maxBoatCrossings)])
 
-# [And(Not(leftSide[t - 1][b]), leftSide[t][b]) for b in range(beingsTotal)]
-# if boat not on the side for the timestep, nothing can switch to true
-# solver.add([Not(Implies(boatOnLeft[t], Not(Or([And(Not(leftSide[t - 1][b]), leftSide[t][b]) for b in range(beingsTotal)])))) for t in range(1, maxBoatCrossings)])
+# if boat did not move to this side at this timestep, nothing can switch to true
+# (basically, only boat movement can allows being movement)
 solver.add([Implies(Not(boatOnLeft[t]), Not(Or([And(Not(leftSide[t - 1][b]), leftSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings)])
 solver.add([Implies(boatOnLeft[t], Not(Or([And(Not(rightSide[t - 1][b]), rightSide[t][b]) for b in range(beingsTotal)]))) for t in range(1, maxBoatCrossings)])
 
 
+# some beings cannot be left alone with others safely
+counter = 0
+for b1 in beingsToCross:
+	print(b1["name"])
+	if b1["cannotOutnumber"] != []:
+		b1Index = getBeingIndexes(b1["name"], beingsToCross)
 
+		for b2 in b1["cannotOutnumber"]:
+			b2Index = getBeingIndexes(b2, beingsToCross)
+
+			solver.add([Sum([leftSide[t][b] for b in b1Index]) <= Sum([leftSide[t][b] for b in b2Index]) for t in range(maxBoatCrossings)])
 
 print(solver.check())
 
@@ -168,3 +151,12 @@ for t in range(1, maxBoatCrossings):
 
 # all beings must be on the right side by the end
 # solver.add(And(rightSide[-1]))
+
+
+# # only the max weight of beings switch sides (as boat can only hold that much)
+# possibleBoatCombos = []
+# for t in range(1, len(rightSide)):
+# 	for subset in itertools.combinations(leftSide[t], maxPerBoat):
+# 		ind1 = leftSide[t].index(subset[0])
+# 		ind2 = leftSide[t].index(subset[1])
+# 		possibleBoatCombos.append(And(subset[0] == Not(leftSide[t - 1][ind1]), subset[1] == Not(leftSide[t - 1][ind2])))
